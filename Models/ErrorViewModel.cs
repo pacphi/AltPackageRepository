777d72b803a7ff239accecf70de4f72e6f49e7d0
Martez Killens <martezkillens@gmail.com>
2021-11-16 13:19:05 -0700

    initial check in

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AltPackageSource.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
